94bff3b59235f72a6b28560d70a8a895ba356221
Alex Jones <alexbeckjones@gmail.com>
2026-08-13 09:02:17 +0100

    Completed SQL ORDER BY exercises

---
+++ sql/2.1_order_by.sql
@@ -0,0 +1,23 @@
+SELECT *
+FROM events
+ORDER BY ticket_price;
+
+SELECT * 
+FROM events
+ORDER BY ticket_price DESC;
+
+SELECT *
+FROM events
+ORDER BY ticket_price ASC;
+
+SELECT *
+FROM events
+ORDER BY event_duration DESC;
+
+SELECT * 
+FROM events 
+ORDER BY number_of_DJs DESC;
+
+SELECT event_name, ticket_price
+FROM events
+ORDER BY ticket_price DESC;
