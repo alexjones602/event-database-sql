f5be8e1ac6ee92c9f32fd3579cc73971f4bc6cf4
Alex Jones <alexbeckjones@gmail.com>
2026-08-13 09:12:55 +0100

    Completed Aggregate Function exercises

---
+++ sql/2.2_aggregate_functions.sql
@@ -0,0 +1,26 @@
+SELECT COUNT(*)
+FROM events;
+
+SELECT AVG(ticket_price)
+FROM events;
+
+SELECT MAX(ticket_price)
+FROM events;
+
+SELECT MIN(ticket_price)
+FROM events;
+
+SELECT AVG(event_duration)
+FROM events;
+
+SELECT MAX(event_duration)
+FROM events;
+
+SELECT MIN(event_duration)
+FROM events;
+
+SELECT SUM(number_of_DJs)
+FROM events;
+
+SELECT AVG(ticket_price) AS average_ticket_price
+FROM events;
